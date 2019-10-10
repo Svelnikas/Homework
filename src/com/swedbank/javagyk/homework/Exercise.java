ed3ce19c4d3c3504722f895c5b7c52e92639d7fc
Antanas Svelnikas <antanassvelnikas@Antanass-MacBook-Air.local>
2019-10-10 22:33:28 +0300

    Homework

---
+++ src/com/swedbank/javagyk/homework/Exercise.java
@@ -0,0 +1,24 @@
+package com.swedbank.javagyk.homework;
+
+import com.sun.org.apache.xpath.internal.objects.XString;
+
+public class Exercise {
+
+    public static void main(String[] args) {
+
+
+        String First_Name;
+                First_Name = "Antanas";
+
+        String Last_Name;
+                Last_Name = "Svelnikas";
+
+        String Job_Role;
+                Job_Role = "Business analyst";
+
+        System.out.println("Hello, I'm "+ First_Name +" "+ Last_Name + ", I'm " + Job_Role);
+
+
+        //Your code should be here :)
+    }
+}
